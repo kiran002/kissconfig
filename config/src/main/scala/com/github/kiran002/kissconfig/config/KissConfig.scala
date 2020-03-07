ded69561871e3cb562d54a74ec73efc4fa6bd32f
Sai Kiran <austin.kiran@gmail.com>
2020-03-07 13:46:21 +0100

    removed redundant fields and methods

--- config/src/main/scala/com/github/kiran002/kissconfig/config/KissConfig.scala
+++ config/src/main/scala/com/github/kiran002/kissconfig/config/KissConfig.scala
@@ -6,13 +6,10 @@ import com.typesafe.config.Config
 import scala.reflect.runtime.universe._
 import scala.reflect.runtime.{universe => ru}
 
-
-
 object KissConfig {
 
   def get[T: TypeTag](config: Config):T = {
-    val tuplesOfFields: List[(String, ru.Type)] = Helper.classAccessors[T]
     val appConfig = ru.typeOf[T].typeSymbol.asClass
-    Helper.get(config, tuplesOfFields, appConfig, ru.typeOf[T]).asInstanceOf[T]
+    Helper.get(config, appConfig, ru.typeOf[T]).asInstanceOf[T]
   }
 }
--- config/src/main/scala/com/github/kiran002/kissconfig/config/internal/Helper.scala
+++ config/src/main/scala/com/github/kiran002/kissconfig/config/internal/Helper.scala
@@ -5,16 +5,12 @@ import com.typesafe.config.Config
 import scala.reflect.runtime.universe._
 import scala.reflect.runtime.{universe => ru}
 
-
 object Helper {
 
-  def classAccessors[T: TypeTag]: List[(String, ru.Type)] =  getListOfFields(typeOf[T])
-
   def get(config: Config,
-          tuplesOfFields: List[(String, ru.Type)],
           classSymbol: ru.ClassSymbol,
           classType: ru.Type): Any = {
-
+    val tuplesOfFields = getListOfFields(classType)
     val runtimeMirror = ru.runtimeMirror(getClass.getClassLoader)
     val classMirror = runtimeMirror.reflectClass(classSymbol)
     val constructorSymbol = classType.decl(ru.termNames.CONSTRUCTOR).asMethod
@@ -23,19 +19,17 @@ object Helper {
     val p = new TypeHelper(config)
 
     val seqOfConfigValues = tuplesOfFields.map { nameTypeTuple =>
-
       val classSymbol = nameTypeTuple._2.typeSymbol.asClass
       p.func.isDefinedAt(nameTypeTuple) match {
         case true => p.func(nameTypeTuple)
         case false if classSymbol.isCaseClass => // must be custom type
-          val tupleOfFields = getListOfFields(nameTypeTuple._2)
-          this.get(config.getConfig(nameTypeTuple._1), tupleOfFields, classSymbol, nameTypeTuple._2)
+          this.get(config.getConfig(nameTypeTuple._1), classSymbol, nameTypeTuple._2)
       }
     }
     constructorMirror(seqOfConfigValues: _*)
   }
 
-  private def getListOfFields(ip:ru.Type) = ip.members.sorted.collect {
-    case m: MethodSymbol if m.isCaseAccessor =>              (m.name.toString, m.returnType)
+  private def getListOfFields(ip: ru.Type) = ip.members.sorted.collect {
+    case m: MethodSymbol if m.isCaseAccessor => (m.name.toString, m.returnType)
   }
 }
