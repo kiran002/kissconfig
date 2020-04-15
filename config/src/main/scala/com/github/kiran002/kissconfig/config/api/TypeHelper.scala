c961e8f8a32f02548871fef5807d1f56b3afa160
Sai Kiran <austin.kiran@gmail.com>
2020-04-15 19:13:30 +0200

    API documentation for TypeHelper

--- config/src/main/scala/com/github/kiran002/kissconfig/config/api/TypeHelper.scala
+++ config/src/main/scala/com/github/kiran002/kissconfig/config/api/TypeHelper.scala
@@ -5,22 +5,51 @@ import com.typesafe.config.Config
 import scala.collection.mutable.ListBuffer
 import scala.reflect.runtime.universe.Type
 
+/**
+  * Interface used to define a [[TypeHelper]].
+  *
+  *  Given a type (T), the TypeHelper is expected to be able
+  *
+  *      1. Extract configuration value from the config object
+  *      2. Parse the value to the target type
+  *
+  */
 trait TypeHelper {
 
+  /**
+    *  Is the typehelper able to handle this particular type ([[objType]])
+    * @param objType: type of the object
+    * @return : function, that takes config object and config key as input and returns the extracted value
+    */
   def canHandle(objType: Type): Boolean
 
   def get(objType: Type): (Config, Option[String]) => Any
 
 }
 
+/**
+  *  Static Wrapper for [[TypeHelper]] that can used to register new implementation of
+  *  TypeHelper during runtime
+  *
+  *  Any Number of TypeHelper can be registered, the helper registered latest has the highest precedence when executing
+  *
+  */
 object TypeHelper {
 
   private val listBuffer: ListBuffer[TypeHelper] = ListBuffer()
 
-  def register(tt: TypeHelper): Unit = {
-    listBuffer += tt
+  /**
+    * Register new [[TypeHelper]]
+    * @param typeHelper
+    */
+  def register(typeHelper: TypeHelper): Unit = {
+    listBuffer += typeHelper
   }
 
-  def get: List[TypeHelper] = listBuffer.toList
+  /**
+    * Get all registered TypeHelpers
+    * @return
+    */
+  def get: List[TypeHelper] = listBuffer.toList.reverse
 
 }
